27df5ecdb862cf3fade75264cf7166958859b903
youlingzhanshi <x_man5888@163.com>
2018-05-17 18:22:13 +0800

    这是一个新的开始！

--- TestCode/FirstPythonCode.py
+++ TestCode/FirstPythonCode.py
@@ -9,7 +9,7 @@
 其实只需要把utf-8转化为unicode编码就可以，因为当windows的CMD遇到unicode编码时，
 会自动给你转化为gbk编码。
 另外python3x中没有unicode类型，但可以支持从utf-8直接解码转换为gbk。
-
+"""
 
 content = "你好，世界！"
 #将字符串由utf-8解码为unicode
@@ -29,7 +29,7 @@ concent = "请输入内容："
 #将字符串由utf-8解码为gbk
 c_gbk = concent.decode("gbk")
 print (c_gbk)
-"""
+
 
 
 
--- TestCode/looptest.py
+++ TestCode/looptest.py
@@ -61,3 +61,48 @@ else:
 # 冒泡排序算法
 # arr = random.sample(range(1,100),20)
 arr =[1,9,3,5,4,8,0]
+for i in range(1,len(arr)+1):
+    for j in range(1,len(arr)-i+1):
+        if arr[j-1] > arr[j]:
+            temp = arr[j]
+            arr[j] = arr[j-1]
+            arr[j-1] = temp
+print '排序之后的顺序：',arr
+
+#插入排序算法
+arr = random.sample(range(1,100),10)
+print arr
+for i in range(1,len(arr)):
+    temp = arr[i]
+    j=i
+    while (j>0 and arr[j-1]>temp):
+        arr[j]=arr[j-1]
+        j-=1
+    arr[j]=temp
+print arr
+
+
+def InsertSort(myList):
+    # 获取列表长度
+    length = len(myList)
+
+    for i in range(1, length):
+        j = i - 1  # 设置当前值前一个元素的标识
+        if (myList[i] < myList[j]):# 如果当前值小于前一个元素,则将当前值作为一个临时变量存储,将前一个元素后移一位
+            temp = myList[i]
+            myList[i] = myList[j]
+            j = j - 1 # 继续往前寻找,如果有比临时变量大的数字,则后移一位,直到找到比临时变量小的元素或者达到列表第一个元素
+            while j >= 0 and myList[j] > temp:
+                myList[j + 1] = myList[j]
+                j = j - 1
+
+            # 将临时变量赋值给合适位置
+            myList[j + 1] = temp
+
+
+myList = [49, 38, 65, 97, 76, 13, 27, 49]
+InsertSort(myList)
+print(myList)
+
+
+
